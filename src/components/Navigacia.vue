db009cb3cb159f6e4cff61be6e67908a78fc8cb2
AlbinaGrigoryan <59582336+AlbinaGrigoryan@users.noreply.github.com>
2021-09-20 15:06:52 +0400

    Update Navigacia.vue

--- src/components/Navigacia.vue
+++ src/components/Navigacia.vue
@@ -1,13 +1,38 @@
 <template>
-$END$
+  <div class="dflex align_center  container_nav">
+    <span :class="['silver', {'blue': count==1}]">Доставка</span>
+    <img src="@/assets/images/arrow.png" alt="arrow">
+    <span :class="['silver', {'blue': count==2}]">Оплата</span>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Navigacia"
+  props: ['count'],
+  data() {
+    return {
+      name: "Navigacia",
+
+    }
+  },
+
+  methods: {},
 }
 </script>
 
 <style scoped>
+.container_nav {
+  padding: 20px 40px;
+  margin-bottom: 33px;
+}
 
+.container_nav span {
+  margin: 0 7px;
+  cursor: pointer;
+  color: #979797;
+}
+
+.container_nav img {
+  margin: 0 5px;
+}
 </style>
